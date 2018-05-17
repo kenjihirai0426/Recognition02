a4889c69bc0fc9023194920d256ee9e0edb59fed
J16016 <J16016@J16016-PC>
2018-05-17 12:43:59 +0900

    first commt

---
+++ src/recognition02/Recognition02_main.java
@@ -0,0 +1,36 @@
+package recognition02;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.Arrays;
+
+import com.ibm.watson.developer_cloud.visual_recognition.v3.VisualRecognition;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifiedImages;
+import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifyOptions;
+
+public class Recognition02_main {
+	public static void main(String[] args){
+
+		VisualRecognition service = new VisualRecognition("2018-03-19");
+		service.setApiKey("j16016");
+
+		InputStream imagesStream = null;
+		try {
+			imagesStream = new FileInputStream("img/fruitbowl.jpg");
+		} catch (FileNotFoundException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+		ClassifyOptions classifyOptions = new ClassifyOptions.Builder()
+		  .imagesFile(imagesStream)
+		  .imagesFilename("fruitbowl.jpg")
+		  .threshold((float) 0.6)
+		  .owners(Arrays.asList("IBM"))
+		  .build();
+		ClassifiedImages result = service.classify(classifyOptions).execute();
+		System.out.println(result);
+
+	}
+
+}
