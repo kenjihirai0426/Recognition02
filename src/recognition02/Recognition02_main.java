0aa7439438f1793efdef229438b16bc3ac906cbb
J16016 <J16016@J16016-PC>
2018-05-24 12:50:11 +0900

    second commit

--- src/recognition02/Recognition02_main.java
+++ src/recognition02/Recognition02_main.java
@@ -2,9 +2,12 @@ package recognition02;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Arrays;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.VisualRecognition;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifiedImages;
 import com.ibm.watson.developer_cloud.visual_recognition.v3.model.ClassifyOptions;
@@ -30,6 +33,43 @@ public class Recognition02_main {
 		  .build();
 		ClassifiedImages result = service.classify(classifyOptions).execute();
 		System.out.println(result);
+
+		String s = String.valueOf(result);
+
+		ObjectMapper mapper = new ObjectMapper();
+
+		try {
+			JsonNode node = mapper.readTree(s);
+			String name = node.get("images").get(0).get("classifiers").get(0).get("name").asText();
+			 System.out.println("name : " + name);
+
+			 String classifier_id  = node.get("images").get(0).get("classifiers").get(0).get("classifier_id").asText();
+			 System.out.println("classifier_id :"+ classifier_id);
+
+			 String classifier_class0 = node.get("images").get(0).get("classifiers").get(0).get("classes").get(0).get("class").asText();
+			 System.out.println("classifier_class0 : " + classifier_class0);
+
+			 float classifier_score0 = node.get("images").get(0).get("classifiers").get(0).get("classes").get(0).get("score").floatValue();
+			 System.out.println("classifier_score0 : " + classifier_score0);
+
+			 String classifier_class1 = node.get("images").get(0).get("classifiers").get(0).get("classes").get(1).get("class").asText();
+			 System.out.println("classifier_class1 : " + classifier_class1);
+
+			 float classifier_score1 = node.get("images").get(0).get("classifiers").get(0).get("classes").get(1).get("score").floatValue();
+			 System.out.println("classifier_score1 : " + classifier_score1);
+
+			 String classifier_class2 = node.get("images").get(0).get("classifiers").get(0).get("classes").get(2).get("class").asText();
+			 System.out.println("classifier_class2 : " + classifier_class2);
+
+			 float classifier_score2 = node.get("images").get(0).get("classifiers").get(0).get("classes").get(2).get("score").floatValue();
+			 System.out.println("classifier_score2 : " + classifier_score2);
+
+
+
+		} catch (IOException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
 
 	}
 
